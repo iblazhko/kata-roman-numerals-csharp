6b120b12c412fa2a201a34e2ba5c8c8a30e58414
Ivan Blazhko <ivan.blazhko@gmail.com>
2018-09-23 14:05:42 +0100

    (Green): 5 converts to V

--- RomanNumerals/RomanNumeralsExtensions.cs
+++ RomanNumerals/RomanNumeralsExtensions.cs
@@ -1,10 +1,18 @@
+using System.Collections.Generic;
+
 namespace RomanNumerals
 {
     public static class RomanNumeralsExtensions
     {
+        private static readonly Dictionary<int, string> numerals = new Dictionary<int, string>
+        {
+            { 1, "I" },
+            { 5, "V" }
+        };
+
         public static string ToRomanNumerals(this int number)
         {
-            return number > 0 ? "I" : string.Empty;
+            return number > 0 ? numerals[number] : string.Empty;
         }
     }
 }
