964b113972737d675b045f9531004bf43b0a4116
Ivan Blazhko <ivan.blazhko@gmail.com>
2018-09-23 14:23:05 +0100

    (Refactor): 2 converts to II
    Generalized the way numerals are constructed from the number

--- RomanNumerals/RomanNumeralsExtensions.cs
+++ RomanNumerals/RomanNumeralsExtensions.cs
@@ -1,19 +1,47 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace RomanNumerals
 {
     public static class RomanNumeralsExtensions
     {
-        private static readonly Dictionary<int, string> numerals = new Dictionary<int, string>
+        private struct Bucket
         {
-            { 1, "I" },
-            { 2, "II" },
-            { 5, "V" }
-        };
+            public int Value { get; }
+            public string Numerals { get; }
+
+            public Bucket(int value, string numeral)
+            {
+                Value = value;
+                Numerals = numeral;
+            }
+        }
+
+        private static readonly List<Bucket> Buckets =
+            new[]
+            {
+                new Bucket(1, "I"),
+                new Bucket(5, "V")
+            }
+            .OrderByDescending(x => x.Value)
+            .ToList();
+
+        private static Bucket FindLagestFullBucket(int number) =>
+            Buckets.First(x => x.Value <= number);
 
         public static string ToRomanNumerals(this int number)
         {
-            return number > 0 ? numerals[number] : string.Empty;
+            var numerals = new StringBuilder();
+            var reminder = number;
+            while (reminder > 0)
+            {
+                var bucket = FindLagestFullBucket(reminder);
+                reminder -= bucket.Value;
+                numerals.Append(bucket.Numerals);
+            }
+
+            return numerals.ToString();
         }
     }
 }
